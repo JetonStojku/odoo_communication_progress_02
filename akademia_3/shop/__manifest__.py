3acb62df21a25de297817efc97f6fe4a8fc47b20
js-flowzar <jstojku@flowzar.com>
2025-01-21 20:04:45 +0100

    odoo start project

---
+++ akademia_3/shop/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'CP Shop',
+    'version': '1.0',
+    'summary': 'CP Shop Summery',
+    'description': 'CP Shop Description',
+    'category': 'Sales',
+    'author': 'Jeton',
+    'website': 'Website',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ akademia_3/shop/models/client.py
@@ -0,0 +1,12 @@
+from odoo import fields, models, api
+
+
+class ShopClient(models.Model):
+    _name = 'shop.client'
+    _description = 'Shop Client'
+
+    name = fields.Char(string='Client Name', required=True)
+    surname = fields.Char(string='Client Surname', required=True)
+    points = fields.Integer(string='Points', required=True, default=0)
+    phone_number = fields.Char(string='Phone number', required=True)
+    email = fields.Char(string='Email')
