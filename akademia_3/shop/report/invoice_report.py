8e5a1cb6d4a90447a21d1e20e203defe2bcdc919
js-flowzar <jetonstojku@hotmail.com>
2025-03-13 19:44:45 +0100

    reports part 2

--- akademia_3/shop/report/invoice_report.py
+++ akademia_3/shop/report/invoice_report.py
@@ -1,37 +1,35 @@
-from odoo import fields, models, api
+from odoo import models, api
 
 
-class CommprogReportInvoice(models.AbstractModel):
+class ShopReportInvoice(models.AbstractModel):
     _name = 'report.shop.report_invoice'
     _description = 'Invoice report'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        domain = [('date', '>=', data['start_date']), ('date', '<', data['end_date']), ('invoice_id.invoice_type', '=', 'out')]
+        domain = [('invoice_id.date', '>=', data['start_date']), ('invoice_id.date', '<', data['end_date'])]
 
         if not data['product_ids']:
-            name = 'Te gjitha produktet'
+            name = 'All products'
         else:
             domain.append(('product_id', 'in', data['product_ids']))
-            produktet = self.env['commprog.product'].browse(data['product_ids']).mapped("name")
-            name = f'Produktet {", ".join(produktet)}'
+            produktet = self.env['shop.product'].browse(data['product_ids']).mapped("name")
+            name = f'Products: {", ".join(produktet)}.'
 
-        rreshta_fature_obj = self.env['commprog.invoice.line'].search(domain)
-        rreshta_fature = {}
+        invoice_line_object = self.env['shop.invoice.line'].search(domain)
+        invoice_lines = []
 
-        for rec in rreshta_fature_obj:
-            if rec.product_id.id not in rreshta_fature:
-                rreshta_fature[rec.product_id.id] = {
-                    'product': rec.product_id.name,
-                    'quantity': rec.product_id.quantity,
-                    'sell': rec.quantity
-                }
-            else:
-                rreshta_fature[rec.product_id.id]['sell'] += rec.quantity
+        for invoice_line in invoice_line_object:
+            invoice_lines.append({
+                'product': invoice_line.product_id.name,
+                'date': invoice_line.invoice_id.date,
+                'quantity': invoice_line.quantity,
+                'price': invoice_line.price,
+                'total': invoice_line.total,
+                'type': 'OUT' if invoice_line.invoice_id.selling_invoice else 'IN',
+            })
 
-        res = sorted(rreshta_fature, key=lambda x: x['sell'], reverse=True)
-        res = []
         return {
-            'docs': res,
+            'docs': invoice_lines,
             'r_name': name,
         }
--- akademia_3/shop/wizard/invoice_report_wizard.py
+++ akademia_3/shop/wizard/invoice_report_wizard.py
@@ -8,3 +8,12 @@ class InvoiceReport(models.TransientModel):
     product_ids = fields.Many2many(comodel_name='shop.product', string='Product')
     start_date = fields.Date(string='Start date', required=True)
     end_date = fields.Date(string='End date', required=True)
+
+    def print_report(self):
+        self.ensure_one()
+        data = {
+            "product_ids": self.product_ids.ids,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+        }
+        return self.env.ref('shop.action_report_invoice').report_action(None, data=data)
