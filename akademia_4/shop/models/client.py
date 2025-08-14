805149c2921d158109699db00cba46d98f990a4e
js-flowzar <jetonstojku@hotmail.com>
2025-08-14 19:10:01 +0200

    create client medel

---
+++ akademia_4/shop/models/client.py
@@ -0,0 +1,19 @@
+from odoo import fields, models
+
+
+class ShopClient(models.Model):
+    _name = 'shop.client'
+
+    name = fields.Char(string='Name', required=True)
+    surname = fields.Char(string='Surname', required=True)
+    birthday = fields.Date(string='Birthday')
+    address = fields.Char(string='Address')
+    client_id = fields.Char(string='Client ID', required=True)
+    point = fields.Integer(string='Points', default=0)
+    email = fields.Char(string='Email')
+    phone_number = fields.Char(string='Phone number')
+    gender = fields.Selection(selection=[
+        ('male', 'Male'),
+        ('female', 'Female')
+        ('other', 'Other')
+    ], string='Gender')
