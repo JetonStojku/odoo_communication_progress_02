d4e16a80ef0548b499bd8141a3440d430dc71a93
js-flowzar <jetonstojku@hotmail.com>
2025-10-15 19:53:19 +0200

    create odoo app

---
+++ akademia_5/market/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Market',
+    'version': '1.0',
+    'summary': 'Brief description of the module',
+    'description': '''
+        Detailed description of the module
+    ''',
+    'category': 'akademia5',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base'],
+    'data': [],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
